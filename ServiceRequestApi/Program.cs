af1e1b15b8f5174bec4fee323a9998f6e6fbf5fe
Chamano Ramashia <chamano.sr@gmail.com>
2026-01-25 11:21:45 +0200

    registering the db context

--- ServiceRequestApi/Program.cs
+++ ServiceRequestApi/Program.cs
@@ -1,4 +1,16 @@
+
+using Microsoft.EntityFrameworkCore;
+using ServiceRequestApi.Infrastructure.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
