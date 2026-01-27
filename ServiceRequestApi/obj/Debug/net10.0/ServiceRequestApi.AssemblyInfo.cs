3b0dec99c63313ab8be03679a6e39fc38de36a88
Chamano Ramashia <chamano.sr@gmail.com>
2026-01-27 19:03:29 +0200

    creating user dto

--- ServiceRequestApi/obj/Debug/net10.0/ServiceRequestApi.AssemblyInfo.cs
+++ ServiceRequestApi/obj/Debug/net10.0/ServiceRequestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceRequestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+162beb4d3f5d12968094a8d78620ba4b5587df4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12393b4536266fb5bee47647486a1fc38037bf08")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceRequestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceRequestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
