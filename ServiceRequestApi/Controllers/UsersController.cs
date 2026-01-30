5ab56a267b5ea4f8e65ecda3c027c7872d53fc45
Chamano Ramashia <chamano.sr@gmail.com>
2026-01-30 09:12:59 +0200

    refactoring the user controller

--- ServiceRequestApi/Controllers/UsersController.cs
+++ ServiceRequestApi/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-using ServiceRequestApi.Infrastructure.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ServiceRequestApi.Contracts.DTOs.Users;
+using ServiceRequestApi.Services.Users;
 
 namespace ServiceRequestApi.Controllers
 {
@@ -7,11 +10,37 @@ namespace ServiceRequestApi.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IUserService _userService;
 
-        public UsersController(ApplicationDbContext context)
+        public UsersController(IUserService userService)
         {
-            _context = context;
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserResponseDto>> CreateUser(CreateUserDto dto)
+        {
+            var createdUser = await _userService.CreateUserAsync(dto);
+
+            return CreatedAtAction(
+                nameof(GetUserById),
+                new { userId = createdUser.UserId },
+                createdUser
+            );
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserResponseDto>>> GetAllUsers()
+        {
+            return Ok(await _userService.GetAllUsersAsync());
+        }
+
+        [HttpGet("{userId:int}")]
+        public async Task<ActionResult<UserResponseDto>> GetUserById(int userId)
+        {
+            var user = await _userService.GetUserByIdAsync(userId);
+            if (user == null) return NotFound();
+            return Ok(user);
         }
     }
 }
