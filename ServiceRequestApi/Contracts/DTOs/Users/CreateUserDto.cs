7f70f2b123e2dcb3689f0d3eb6e82f011c521a0d
Chamano Ramashia <chamano.sr@gmail.com>
2026-02-03 13:35:46 +0200

    adding validation

--- ServiceRequestApi/Contracts/DTOs/Users/CreateUserDto.cs
+++ ServiceRequestApi/Contracts/DTOs/Users/CreateUserDto.cs
@@ -1,14 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ServiceRequestApi.Contracts.DTOs.Users
 {
     public class CreateUserDto
     {
-        public string Username { get; set; }   
-        public string Password { get; set; }   
+        [Required, MinLength(3)]
+        public string Username { get; set; }
 
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required, EmailAddress]
         public string Email { get; set; }
 
+        [Range(1, int.MaxValue)]
         public int DepartmentId { get; set; }
     }
 }
